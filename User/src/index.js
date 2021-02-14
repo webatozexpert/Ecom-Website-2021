4a3df59163a93674e50c131f7e2c62fa4f76e60e
webatozexpert <webatozexpert@gmail.com>
2021-02-14 22:29:36 +0600

    Install starting dependecies and create starting file folder
     Install starting dependecies and create starting file folder

--- User/src/index.js
+++ User/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/assets/css/style.css';
+import '../src/assets/css/fontawesome.css';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
