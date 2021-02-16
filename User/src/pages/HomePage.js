9149669a401b7e95238b72f7aae7f1501abec532
webatozexpert <webatozexpert@gmail.com>
2021-02-16 22:21:29 +0600

    Accordion Menu Component
     Accordion Menu Component

--- User/src/pages/HomePage.js
+++ User/src/pages/HomePage.js
@@ -1,9 +1,10 @@
 import React, {Component,Fragment} from 'react';
+import {Card,Button,Container,Row,Col} from "react-bootstrap";
 import FeaturedProducts from "../components/home/FeaturedProducts";
 import Categories from "../components/home/Categories";
 import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
-
+import HomeTop from "../components/home/HomeTop";
 
 
 class HomePage extends Component {
@@ -11,7 +12,7 @@ class HomePage extends Component {
         return (
             <Fragment>
 
-
+                <HomeTop/>
                 <NewArrival/>
                 <FeaturedProducts/>
                 <Collection/>
