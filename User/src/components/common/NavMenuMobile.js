3beddd508e4d2024130c0543addb6974227b7ded
webatozexpert <webatozexpert@gmail.com>
2021-02-18 23:30:03 +0600

    Mobile View Menu
    Mobile View Menu

--- User/src/components/common/NavMenuMobile.js
+++ User/src/components/common/NavMenuMobile.js
@@ -1,11 +1,61 @@
 import React, {Component,Fragment} from 'react';
+import {Button, Col, Container, Navbar, Row} from "react-bootstrap";
+import MegaMenu from "../home/MegaMenu";
+import MegaMenuMobile from "../home/MegaMenuMobile";
 
 class NavMenuMobile extends Component {
+
+    constructor() {
+        super();
+        this.state={
+            SideNavState:"sideNavClose",
+            ContentOverState:"ContentOverlayClose"
+        }
+    }
+
+
+    MenuBarClickHandler=()=>{
+        this.SideNavOpenClose();
+    }
+
+    ContentOverlayClickHandler=()=>{
+        this.SideNavOpenClose();
+    }
+
+
+
+    SideNavOpenClose=()=>{
+        let SideNavState= this.state.SideNavState;
+        let ContentOverState= this.state.ContentOverState;
+        if(SideNavState==="sideNavOpen"){
+            this.setState({SideNavState:"sideNavClose",ContentOverState:"ContentOverlayClose"})
+        }
+        else{
+            this.setState({SideNavState:"sideNavOpen",ContentOverState:"ContentOverlayOpen"})
+        }
+    }
+
+
+
+
     render() {
         return (
             <Fragment>
+                <Navbar fluid={"true"} className="fixed-top shadow-sm p-2 m-0 bg-white">
+                    <a onClick={this.MenuBarClickHandler} className=" mx-2 navbar-brand"><i className="fa fa-bars"></i></a>
+                    <a href="" className="btn"> <img className="nav-logo" src="http://demo.ecom.rabbil.com/static/media/BigExpress.432afd37.png"/></a>
+                    <Button className="cart-btn"><i className="fa fa-shopping-cart"></i> 4 items </Button>
+                </Navbar>
+                <div  className={this.state.SideNavState}>
+                    <MegaMenuMobile/>
+                </div>
+
+                <div onClick={this.ContentOverlayClickHandler}  className={this.state.ContentOverState}>
+
+                </div>
 
             </Fragment>
+
         );
     }
 }
--- User/src/pages/HomePage.js
+++ User/src/pages/HomePage.js
@@ -6,18 +6,42 @@ import Collection from "../components/home/Collection";
 import NewArrival from "../components/home/NewArrival";
 import HomeTop from "../components/home/HomeTop";
 import NavMenuDesktop from "../components/common/NavMenuDesktop";
+import NavMenuMobile from "../components/common/NavMenuMobile";
+import HomeTopMobile from "../components/home/HomeTopMobile";
+import FooterDesktop from "../components/common/FooterDesktop";
+import FooterMobile from "../components/common/FooterMobile";
 
 
 class HomePage extends Component {
     render() {
         return (
             <Fragment>
-                <NavMenuDesktop/>
-                <HomeTop/>
+
+                <div className="Desktop">
+                    <NavMenuDesktop/>
+                    <HomeTop/>
+                </div>
+
+                <div className="Mobile">
+                    <NavMenuMobile/>
+                    <HomeTopMobile/>
+                </div>
+
                 <NewArrival/>
                 <FeaturedProducts/>
                 <Collection/>
                 <Categories/>
+
+                <div  className="Desktop">
+                    <FooterDesktop/>
+                </div>
+
+                <div  className="Mobile">
+                    <FooterMobile/>
+                </div>
+
+
+
             </Fragment>
         );
     }
