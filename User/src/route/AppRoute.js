b1200ad12f8c468674810c6f73db7c223a4b97ed
webatozexpert <webatozexpert@gmail.com>
2021-02-20 10:25:16 +0600

    Other's Page
    Other's Page

--- User/src/route/AppRoute.js
+++ User/src/route/AppRoute.js
@@ -3,6 +3,12 @@ import {Route, Switch} from "react-router";
 import HomePage from "../pages/HomePage";
 import UserOnboardPage from "../pages/UserOnboardPage";
 import ContactPage from "../pages/ContactPage";
+import PurchasePage from "../pages/PurchasePage";
+import PolicyPage from "../pages/PolicyPage";
+import RefundPage from "../pages/RefundPage";
+import AboutPage from "../pages/AboutPage";
+
+
 
 class AppRoute extends Component {
     render() {
@@ -13,6 +19,11 @@ class AppRoute extends Component {
                     <Route exact path="/" component={HomePage}/>
                     <Route exact path="/onboard" component={UserOnboardPage}/>
                     <Route exact path="/contact" component={ContactPage}/>
+                    <Route exact path="/purchase" component={PurchasePage}/>
+                    <Route exact path="/policy" component={PolicyPage}/>
+                    <Route exact path="/refund" component={RefundPage}/>
+                    <Route exact path="/about" component={AboutPage}/>
+
                 </Switch>
 
             </Fragment>
