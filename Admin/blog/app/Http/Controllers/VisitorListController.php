fc695e0cdda0dcacbdd5470d8b62085515686a95
webatozexpert <webatozexpert@gmail.com>
2021-02-24 21:12:54 +0600

    SendVisitorDetails
    SendVisitorDetails

---
+++ Admin/blog/app/Http/Controllers/VisitorListController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\visitorModel;
+use Illuminate\Http\Request;
+
+class VisitorListController extends Controller
+{
+    function SendVisitorDetails(Request $request){
+        $ip_address=$_SERVER['REMOTE_ADDR'];
+        date_default_timezone_set("Asia/Dhaka");
+        $visit_time= date("h:i:sa");
+        $visit_date= date("d-m-Y");
+        $result=visitorModel::insert([
+            'ip_address'=>$ip_address,
+            'visit_time'=>$visit_time,
+            'visit_date'=>$visit_date,
+        ]);
+        return $result;
+    }
+}
