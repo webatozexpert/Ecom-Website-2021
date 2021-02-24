cd3a0ed81145b6aa68392c6a3c334986be755263
webatozexpert <webatozexpert@gmail.com>
2021-02-24 23:59:06 +0600

    Consume Visitor Details API Form Client
    Consume Visitor Details API Form Client

---
+++ User/src/api/ApiURL.js
@@ -0,0 +1,6 @@
+class ApiURL{
+    static BaseURL="http://127.0.0.1:8000/api/"
+
+ static VisitorDetails=this.BaseURL+"SendVisitorDetails"
+}
+export default ApiURL;
--- User/src/pages/HomePage.js
+++ User/src/pages/HomePage.js
@@ -10,9 +10,24 @@ import NavMenuMobile from "../components/common/NavMenuMobile";
 import HomeTopMobile from "../components/home/HomeTopMobile";
 import FooterDesktop from "../components/common/FooterDesktop";
 import FooterMobile from "../components/common/FooterMobile";
-
+import axios from "axios";
+import ApiURL from "../api/ApiURL";
 
 class HomePage extends Component {
+
+
+
+    componentDidMount() {
+        window.scroll(0,0);
+        this.SendVisitorDetails();
+    }
+
+
+    SendVisitorDetails=()=>{
+    axios.get(ApiURL.VisitorDetails).then().catch();
+
+    }
+
     render() {
         return (
             <Fragment>
