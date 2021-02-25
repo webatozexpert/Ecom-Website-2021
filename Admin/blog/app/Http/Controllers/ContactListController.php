29c194fb32897ce6987d66d1f623e78c018f945e
webatozexpert <webatozexpert@gmail.com>
2021-02-25 21:17:14 +0600

    Create Contact Rest API
    Create Contact Rest API

---
+++ Admin/blog/app/Http/Controllers/ContactListController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ContactModel;
+use Illuminate\Http\Request;
+
+class ContactListController extends Controller
+{
+    function SendContactDetails(Request $request){
+        $name=$request->input('name');
+        $mobile=$request->input('mobile');
+        $message=$request->input('message');
+        $contact_time= date("h:i:sa");
+        $contact_date= date("d-m-Y");
+
+        $result= ContactModel::insert([
+            'name'=>$name,
+            'mobile'=>$mobile,
+            'message'=>$message,
+            'contact_date'=>$contact_date,
+            'contact_time'=>$contact_time
+        ]);
+
+        return $result;
+    }
+}
--- Admin/blog/routes/api.php
+++ Admin/blog/routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+use App\Http\Controllers\ContactListController;
 use App\Http\Controllers\VisitorListController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/SendVisitorDetails',[VisitorListController::class,'SendVisitorDetails']);
+Route::post('/SendContactDetails',[ContactListController::class,'SendContactDetails']);
